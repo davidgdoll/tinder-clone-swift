f7925b0aa1d78da704590eaf0bba00fb31c16965
David Doll <davidgrdoll@gmail.com>
2019-05-09 01:14:56 -0300

    Change StackView distribuition

--- TinderClone/Views/HomeBottomControlsStackView.swift
+++ TinderClone/Views/HomeBottomControlsStackView.swift
@@ -12,17 +12,13 @@ class HomeBottomControlsStackView: UIStackView {
  
     override init(frame: CGRect) {
         super.init(frame: frame)
-        distribution = .fillEqually
+        distribution = .fill
         heightAnchor.constraint(equalToConstant: 90).isActive = true
         
-        let subViews = [#imageLiteral(resourceName: "refresh_circle"), #imageLiteral(resourceName: "dismiss_circle"), #imageLiteral(resourceName: "super_like_circle"), #imageLiteral(resourceName: "like_circle"), #imageLiteral(resourceName: "boost_circle")].map { img -> UIView in
+        [#imageLiteral(resourceName: "refresh_circle"), #imageLiteral(resourceName: "dismiss_circle"), #imageLiteral(resourceName: "super_like_circle"), #imageLiteral(resourceName: "like_circle"), #imageLiteral(resourceName: "boost_circle")].forEach { img in
             let button = UIButton(type: .system)
             button.setImage(img.withRenderingMode(.alwaysOriginal), for: .normal)
-            return button
-        }
-        
-        subViews.forEach { v in
-            addArrangedSubview(v)
+            addArrangedSubview(button)
         }
     }
     
