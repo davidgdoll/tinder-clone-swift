4a38ced22e14269bc526d1f4a1eb176426e30ff0
David Doll <davidgrdoll@gmail.com>
2019-05-09 01:14:32 -0300

    Add CardView layout

---
+++ TinderClone/Views/CardView.swift
@@ -0,0 +1,51 @@
+//
+//  CardView.swift
+//  TinderClone
+//
+//  Created by David Doll on 09/05/19.
+//  Copyright © 2019 David Doll. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let image = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        addSubview(image)
+        image.fillSuperview()
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded()
+        default:
+            ()
+        }
+    }
+    
+    fileprivate func handleEnded() {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+        }, completion: nil)
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    }
+}
