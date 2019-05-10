4f9d2c28ddf0e51290e1733e6a61d5654cf056e6
David Doll <davidgrdoll@gmail.com>
2019-05-10 01:11:26 -0300

    Add card model objects

---
+++ TinderClone/Model/Advertiser.swift
@@ -0,0 +1,15 @@
+//
+//  Advertiser.swift
+//  TinderClone
+//
+//  Created by David Doll on 10/05/19.
+//  Copyright © 2019 David Doll. All rights reserved.
+//
+
+import UIKit
+
+struct Advertiser {
+    let title: String
+    let brandName: String
+    let posterPhotoName: String
+}
---
+++ TinderClone/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  TinderClone
+//
+//  Created by David Doll on 09/05/19.
+//  Copyright © 2019 David Doll. All rights reserved.
+//
+
+import UIKit
+
+struct User {
+    let name: String
+    let age: Int
+    let profession: String
+    let imageName: String
+}
+
