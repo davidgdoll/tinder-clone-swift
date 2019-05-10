9d619fca80ed305668ec1bf0042d39a1e6504503
David Doll <davidgrdoll@gmail.com>
2019-05-10 01:12:29 -0300

    Introduces viewModel to CardView

--- TinderClone/Views/CardView.swift
+++ TinderClone/Views/CardView.swift
@@ -10,19 +10,37 @@ import UIKit
 
 class CardView: UIView {
     
-    fileprivate let image = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    fileprivate let imageView = UIImageView(frame: .zero)
+    fileprivate let informationLabel = UILabel(frame: .zero)
+    fileprivate let threshold: CGFloat = 100
+    
+    var viewModel: CardViewModelProtocol! {
+        didSet {
+            imageView.image = UIImage(named: viewModel.image)
+            informationLabel.attributedText = viewModel.attributedString
+            informationLabel.textAlignment = viewModel.textAlignment
+        }
+    }
     
     override init(frame: CGRect) {
-        super.init(frame: frame)
+        super.init(frame: .zero)
+
         layer.cornerRadius = 10
         clipsToBounds = true
-        addSubview(image)
-        image.fillSuperview()
+        
+        imageView.contentMode = .scaleAspectFill
+        addSubview(imageView)
+        imageView.fillSuperview()
+
+        informationLabel.textColor = .white
+        informationLabel.numberOfLines = 0
+        addSubview(informationLabel)
+        informationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
         
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         addGestureRecognizer(panGesture)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -32,20 +50,38 @@ class CardView: UIView {
         case .changed:
             handleChanged(gesture)
         case .ended:
-            handleEnded()
+            handleEnded(gesture)
         default:
             ()
         }
     }
     
-    fileprivate func handleEnded() {
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-            self.transform = .identity
-        }, completion: nil)
-    }
-    
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        let degrees = translation.x / 20
+        let angle = degrees * .pi / 180
+        
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
+    }
+    
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        
+        let translationX = gesture.translation(in: nil).x
+        let direction: CGFloat = translationX < 0 ? -1 : 1
+        let shouldDismissCard = abs(translationX) > threshold
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            if shouldDismissCard {
+                self.frame = .init(x: 600 * direction, y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
+            }
+        }, completion: { a in
+            self.transform = .identity
+            if shouldDismissCard {
+                self.removeFromSuperview()
+            }
+        })
     }
 }
