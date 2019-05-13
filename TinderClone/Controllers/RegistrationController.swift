39c5bfe75caf2416933690f86bacdc8f352be344
David Doll <davidgrdoll@gmail.com>
2019-05-12 22:33:32 -0300

    Add custom text field class

---
+++ TinderClone/Controllers/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  TinderClone
+//
+//  Created by David Doll on 12/05/19.
+//  Copyright © 2019 David Doll. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TinderClone/Views/CustomTextField.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTextField.swift
+//  TinderClone
+//
+//  Created by David Doll on 12/05/19.
+//  Copyright © 2019 David Doll. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    let padding: CGFloat
+    
+    init(padding: CGFloat) {
+        self.padding = padding
+        super.init(frame: .zero)
+        layer.cornerRadius = 25
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 0, height: 50)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
