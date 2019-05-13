623ea44be6077b3ae2280ce9c2b61f1539c3b0c6
David Doll <davidgrdoll@gmail.com>
2019-05-12 22:34:11 -0300

    Force delegate to start with RegistrationController

--- TinderClone/AppDelegate.swift
+++ TinderClone/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = RegistrationController()
         return true
     }
 }
