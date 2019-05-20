63f6f484780abc43f831a6354f939aab55c289b8
David Doll <davidgrdoll@gmail.com>
2019-05-19 23:51:29 -0300

    Save user info to Firestore

--- TinderClone/ViewModels/RegistrationViewModel.swift
+++ TinderClone/ViewModels/RegistrationViewModel.swift
@@ -20,32 +20,46 @@ class RegistrationViewModel {
     var isRegistering = Bindable<Bool>()
     var image = Bindable<UIImage>()
     
-    func performRegistration(onError: @escaping (Error?) -> Void) {
+    func performRegistration(completion: @escaping (Error?) -> Void) {
         guard let email = email, let password = password else { return }
         isRegistering.value = true
         
         Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
             if let error = error {
-                onError(error)
+                completion(error)
+            }
+            self?.saveImageToFirebase(completion: completion)
+        }
+    }
+    
+    fileprivate func saveImageToFirebase(completion: @escaping (Error?) -> Void) {
+        let fileName = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/images/\(fileName)")
+        let imageData = image.value?.jpegData(compressionQuality: 0.75) ?? Data()
+        ref.putData(imageData, metadata: nil, completion: { _, error in
+            if let error = error {
+                completion(error)
+                return
             }
             
-            let fileName = UUID().uuidString
-            let ref = Storage.storage().reference(withPath: "/images/\(fileName)")
-            let imageData = self?.image.value?.jpegData(compressionQuality: 0.75) ?? Data()
-            ref.putData(imageData, metadata: nil, completion: { _, error in
+            ref.downloadURL(completion: { [weak self] url, error in
                 if let error = error {
-                    onError(error)
+                    completion(error)
                     return
                 }
-                
-                ref.downloadURL(completion: { url, error in
-                    if let error = error {
-                        onError(error)
-                        return
-                    }
-                    self?.isRegistering.value = false
-                })
+                let imageUrl = url?.absoluteString ?? ""
+                self?.saveInfoToFirestore(imageUrl: imageUrl, completion: completion)
+                self?.isRegistering.value = false
             })
+        })
+    }
+    
+    fileprivate func saveInfoToFirestore(imageUrl: String, completion: @escaping (Error?) -> Void) {
+        let uid = Auth.auth().currentUser?.uid ?? ""
+        let docData = ["fullName": fullName ?? "", "uid": uid, "imageUrl1": imageUrl]
+        Firestore.firestore().collection("users").document(uid).setData(docData) { error in
+            guard error == nil else { completion(error); return }
+            completion(nil)
         }
     }
     
