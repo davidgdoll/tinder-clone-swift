41df5a716da3e80bc1f680ce42a1445f8d880c49
David Doll <davidgrdoll@gmail.com>
2019-07-07 23:40:49 -0300

    Load user settings from Firebase

--- TinderClone/Controllers/SettingsController.swift
+++ TinderClone/Controllers/SettingsController.swift
@@ -6,11 +6,15 @@
 //  Copyright © 2019 David Doll. All rights reserved.
 //
 
-import UIKit
 
 class CustomImagePickerController: UIImagePickerController {
     var imageButton: UIButton?
 }
+
+import UIKit
+import Firebase
+import SDWebImage
+import JGProgressHUD
 
 class SettingsController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -49,6 +53,27 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
         tableView.tableFooterView = UIView()
         tableView.keyboardDismissMode = .interactive
+        fetchCurrentUser()
+    }
+    
+    var user: User?
+    
+    fileprivate func fetchCurrentUser() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        Firestore.firestore().collection("users").document(uid).getDocument { snapshot, error in
+            guard error == nil else { return }
+            guard let data = snapshot?.data() else { return }
+            self.user = User(dictionary: data)
+            self.loadUserPhotos()
+            self.tableView.reloadData()
+        }
+    }
+    
+    fileprivate func loadUserPhotos() {
+        guard let imageUrl = user?.imageUrl1, let url = URL(string: imageUrl) else { return }
+        SDWebImageManager.shared().loadImage(with: url, options: .continueInBackground, progress: nil) { image, _, _, _, _, _ in
+            self.image1Button.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
     }
     
     fileprivate func setupNavigationItems() {
@@ -125,10 +150,15 @@ class SettingsController: UITableViewController, UIImagePickerControllerDelegate
         switch indexPath.section {
         case 1:
             cell.textField.placeholder = "Enter Name"
+            cell.textField.text = user?.name
         case 2:
             cell.textField.placeholder = "Enter Profession"
+            cell.textField.text = user?.profession
         case 3:
             cell.textField.placeholder = "Enter Age"
+            if let age = user?.age {
+                cell.textField.text = String(age)
+            }
         default:
             cell.textField.placeholder = "Enter Bio"
         }
